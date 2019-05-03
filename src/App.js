54794fb7f47155dbecb357efd78326a2c5ced570
erictuftee <erictuftee@gmail.com>
2019-05-03 15:30:59 -0500

    add input to add fields

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import React, {Component} from 'react';
 import './App.css';
-import './styles.scss';
 import {AgGridReact} from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import 'ag-grid-enterprise';
 import {generateMockData} from './generate-mock-data';
+import './styles.scss';
+import Chance from 'chance';
+
+const chance = new Chance();
 
 class App extends Component {
     componentDidMount() {
         const NameList = ['Matt', 'Nathan', 'Kalyan', 'Bill', 'John', 'Asuvani', 'Mark', 'Mike', 'Megan'];
         const generatedData = generateMockData(NameList);
-
-        console.log(generatedData.columnDefs);
-        console.log(this.state.columnDefs);
         this.setState({rowData: generatedData.rowData, columnDefs: generatedData.columnDefs});
     }
 
     constructor(props) {
         super(props);
         this.state = {
-            columnDefs: [
-                {
-                    field: 'keyArea',
-                    headerName: 'Functional Area'
-                }
-            ],
-
-            rowData: [
-                // {keyArea: 'Zeke busicpi oga.', '2eca01a876': 3, '782400d7f2': 5, e7f5633c45: 4},
-                // {keyArea: 'Okeehak sezha rupud.', '2eca01a876': 2, '782400d7f2': 4, e7f5633c45: 5},
-                // {keyArea: 'Peciibo al offasad.', '2eca01a876': 3, '782400d7f2': 0, e7f5633c45: 3},
-                // {keyArea: 'Utbiin reb iffod.', '2eca01a876': 5, '782400d7f2': 4, e7f5633c45: 3}
-                {}
-            ],
+            columnDefs: [],
+            rowData: [{}],
             defaultColDef: {
                 editable: true,
                 resizable: true,
                 sortable: true
-            }
+            },
+            newPerson: '',
+            functionalArea: ''
         };
     }
 
@@ -53,22 +44,112 @@ class App extends Component {
         });
     };
 
+    addPerson = event => {
+        event.preventDefault();
+        //let newColDef = {...this.state.columnDefs[this.state.columnDefs.length - 1]};
+        const newId = chance.hash({length: 10});
+        let newColDef = {
+            headerName: this.state.newPerson,
+            field: newId,
+            valueFormatter: params => {
+                return params.value === 0 ? '' : params.value;
+            },
+            editable: true,
+            cellClass: params => {
+                return `cell-color-${params.value} center-text`;
+            },
+            aggFunc: 'sum'
+        };
+
+        let updatedRowData = this.state.rowData.map(el => {
+            el[newId] = 0;
+            return el;
+        });
+
+        let temp = this.state.columnDefs.concat(newColDef);
+        this.state.gridApi.setColumnDefs(temp);
+        this.setState({rowData: updatedRowData, columnDefs: temp}, () => {
+            this.state.gridApi.sizeColumnsToFit();
+        });
+    };
+
+    addFunctionalArea = event => {
+        event.preventDefault();
+        const newId = chance.hash({length: 10});
+        const newArea = this.state.functionalArea;
+        let updatedRowData = this.state.rowData.map(el => {
+            el[newId] = 0;
+            return el;
+        });
+        let newRowData = {...this.state.rowData[this.state.rowData.length - 1]};
+        newRowData[newId] = 0;
+        Object.keys(newRowData).map(el => {
+            if (el === 'keyArea') {
+                newRowData[el] = newArea;
+            } else if (el !== 'groupHack') {
+                newRowData[el] = 0;
+            }
+        });
+        updatedRowData = updatedRowData.concat(newRowData);
+        this.setState({rowData: updatedRowData});
+    };
+
+    handleFAChange = e => {
+        this.setState({functionalArea: e.target.value});
+    };
+    handleAddPersonChange = e => {
+        this.setState({newPerson: e.target.value});
+    };
+
     render() {
         return (
             <div className='app-container'>
                 <div
                     className='ag-theme-balham'
                     style={{
-                        height: '500px',
+                        height: '90vh',
                         width: '100%'
                     }}
                 >
+                    <div className='actions-container'>
+                        <div className='action'>
+                            <label>
+                                Add Functional Area:
+                                <input type='text' className='action-input' onChange={this.handleFAChange} />
+                                <button onClick={this.addFunctionalArea}>Add</button>
+                            </label>
+                        </div>
+                        <div className='action'>
+                            <label>
+                                Add Person:
+                                <input type='text' className='action-input' onChange={this.handleAddPersonChange} />
+                                <button onClick={this.addPerson}>Add</button>
+                            </label>
+                        </div>
+                    </div>
+
                     <AgGridReact
                         onGridReady={this.onGridReady}
                         columnDefs={this.state.columnDefs}
                         rowData={this.state.rowData}
-                        groupIncludeFooter={true}
-                        // groupIncludeTotalFooter={true}
+                        groupDefaultExpanded={1}
+                        suppressAggFuncInHeader={true}
+                        autoGroupColumnDef={{
+                            headerName: '',
+                            pinned: 'left',
+                            width: 1,
+                            resizable: false,
+                            filter: false,
+                            menuTabs: [],
+                            suppressSizeToFit: true,
+                            checkbox: false,
+                            cellRenderer: 'agGroupCellRenderer'
+                        }}
+                        defaultColDef={{
+                            sortable: true,
+                            resizable: true,
+                            filter: true
+                        }}
                     />
                 </div>
             </div>
--- src/generate-mock-data.js
+++ src/generate-mock-data.js
@@ -3,21 +3,7 @@ import Chance from 'chance';
 const chance = new Chance('test');
 
 export const generateMockData = NameList => {
-    const numOfBaseMachines = 10;
-    const numOfRows = 100;
     let mockData = [];
-    //const NameList = ['Matt', 'Nathan', 'Kalyan'];
-
-    const generatePeople = colList => {
-        let nameList = [];
-        for (var i = 1; i < colList.length; i++) {
-            nameList.push({
-                baseMachineCode: chance.hash({casing: 'upper', length: 6}),
-                baseMachineName: chance.sentence({words: 3})
-            });
-        }
-        return nameList;
-    };
 
     const generateHeaders = NameList => {
         let columnIds = NameList.map(el => chance.hash({length: 10}));
@@ -30,10 +16,34 @@ export const generateMockData = NameList => {
                 editable: true
             },
             {
+                field: 'groupHack',
+                headerName: '',
+                rowGroup: true,
+                pinned: 'left',
+                groupDefaultExpanded: true,
+                hide: true,
+                width: 5,
+                menuTabs: [],
+                sortable: false,
+                resizable: false,
+                filter: false,
+                cellClass: 'groupHack'
+            },
+            {
                 headerName: 'Total',
+                pinned: 'left',
                 valueGetter: params => {
                     let total = 0;
-                    columnIds.map(el => {
+                    const colIds = Object.keys(params.data).map(curr => {
+                        if (curr !== 'keyArea' && curr !== 'y' && curr !== 'groupHack') {
+                            return curr;
+                        } else {
+                            return false;
+                        }
+                    });
+                    console.log(colIds);
+                    var filtered = colIds.filter(el => el);
+                    filtered.map(el => {
                         total += parseInt(params.data[el]);
                     });
                     return total;
@@ -42,33 +52,23 @@ export const generateMockData = NameList => {
                 aggFunc: 'sum',
                 cellClass: 'total-col',
                 suppressSizeToFit: true,
-                width: 60
+                width: 75,
+                menuTabs: [],
+                field: 'y'
             }
         ];
         for (var i = 0; i < NameList.length; i++) {
             let colDef = {};
-            //const id = chance.hash({length: 10});
-            //const id = columnIds[i];
             colDef.headerName = NameList[i];
             colDef.field = columnIds[i];
+            colDef.valueFormatter = params => {
+                return params.value === 0 ? '' : params.value;
+            };
             colDef.editable = true;
             colDef.cellClass = params => {
-                return `cell-color-${params.value}`;
+                return `cell-color-${params.value} center-text`;
             };
-            colDef.children = [
-                {
-                    headerValueGetter: params => {
-                        console.log(params);
-                    },
-                    // aggFunc: 'sum',
-                    // headerName: 'xx',
-                    field: columnIds[i],
-                    editable: true,
-                    cellClass: params => {
-                        return `cell-color-${params.value}`;
-                    }
-                }
-            ];
+            colDef.aggFunc = 'sum';
             defsList.push(colDef);
         }
         return defsList;
@@ -85,261 +85,18 @@ export const generateMockData = NameList => {
     const generateRow = (peopleList, areaName) => {
         let newRow = {};
         for (var i = 0; i < peopleList.length; i++) {
-            newRow[peopleList[i].field] = chance.natural({min: 0, max: 5});
+            const temp = chance.natural({min: 0, max: 15});
+            newRow[peopleList[i].field] = temp > 5 ? 0 : temp;
         }
         newRow.keyArea = areaName;
-        // let newRow = {
-        //     //optionGroupName: chance.sentence({ words: 6 }),
-        //     optionGroupName: groupNameList[Math.floor(Math.random() * (groupNameList.length - 1 - 0) + 0)],
-        //     optionCodeName: chance.sentence({words: 3}),
-        //     optionCode: chance.natural({min: 1000, max: 9999}),
-        //     optionTakeRates: []
-        // };
-        // for (var i = 0; i < modelList.length; i++) {
-        //     newRow.optionTakeRates.push({
-        //         baseMachineCode: modelList[i].baseMachineCode,
-        //         baseMachineName: modelList[i].baseMachineName,
-        //         takeRateCount: chance.natural({min: 0, max: 400}),
-        //         takeRatePercent: chance.floating({min: 0, max: 100, fixed: 1})
-        //     });
-        // }
+        newRow.groupHack = `x`;
         return newRow;
     };
 
-    //const peopleList = generatePeople(NameList);
-    const peopleList = NameList;
     const columnDefs = generateHeaders(NameList);
-    const areaNameList = generateAreaNames(chance.natural({min: 15, max: 20}));
+    const areaNameList = generateAreaNames(chance.natural({min: 15, max: 40}));
     for (var i = 1; i < areaNameList.length; i++) {
         mockData.push(generateRow(columnDefs, areaNameList[i]));
     }
     return {rowData: mockData, columnDefs};
 };
-export const staticMockData = () => {
-    return [
-        {
-            optionGroupName: 'Grand Total',
-            optionCodeName: '',
-            optionCode: '',
-            optionTakeRates: [
-                {
-                    baseMachineCode: '459ENW',
-                    baseMachineName: '4630 Sprayer',
-                    takeRateCount: 258,
-                    takeRatePercent: 100
-                },
-                {
-                    baseMachineCode: '4620NW',
-                    baseMachineName: '4730 Sprayer',
-                    takeRateCount: 453,
-                    takeRatePercent: 100
-                },
-                {
-                    baseMachineCode: '462ENW',
-                    baseMachineName: '4730 Sprayer Carb',
-                    takeRateCount: 138,
-                    takeRatePercent: 100
-                },
-                {
-                    baseMachineCode: '462FNW',
-                    baseMachineName: '4730 Sprayer Carb B',
-                    takeRateCount: 604,
-                    takeRatePercent: 100
-                },
-                {
-                    baseMachineCode: '-1',
-                    baseMachineName: 'Grand Total',
-                    takeRateCount: 1453,
-                    takeRatePercent: 100
-                }
-            ]
-        },
-        {
-            optionGroupName: 'Ag Management Solutions Autotrac Activations',
-            optionCodeName: 'Auto Trac Activation sf1',
-            optionCode: 1841,
-            optionTakeRates: [
-                {
-                    baseMachineCode: '459ENW',
-                    baseMachineName: '4630 Sprayer',
-                    takeRateCount: 69,
-                    takeRatePercent: 26.7
-                },
-                {
-                    baseMachineCode: '4620NW',
-                    baseMachineName: '4730 Sprayer',
-                    takeRateCount: 27,
-                    takeRatePercent: 6.0
-                },
-                {
-                    baseMachineCode: '462ENW',
-                    baseMachineName: '4730 Sprayer Carb',
-                    takeRateCount: 0,
-                    takeRatePercent: 0
-                },
-                {
-                    baseMachineCode: '462FNW',
-                    baseMachineName: '4730 Sprayer Carb B',
-                    takeRateCount: 124,
-                    takeRatePercent: 20.5
-                },
-                {
-                    baseMachineCode: '-1',
-                    baseMachineName: 'Grand Total',
-                    takeRateCount: 220,
-                    takeRatePercent: 15.1
-                }
-            ]
-        },
-        {
-            optionGroupName: 'Ag Management Solutions Autotrac Activations',
-            optionCodeName: 'Auto Trac Activation sf3',
-            optionCode: '1841',
-            optionTakeRates: [
-                {
-                    baseMachineCode: '459ENW',
-                    baseMachineName: '4630 Sprayer',
-                    takeRateCount: 169,
-                    takeRatePercent: 65.5
-                },
-                {
-                    baseMachineCode: '4620NW',
-                    baseMachineName: '4730 Sprayer',
-                    takeRateCount: 32,
-                    takeRatePercent: 7.1
-                },
-                {
-                    baseMachineCode: '462ENW',
-                    baseMachineName: '4730 Sprayer Carb',
-                    takeRateCount: 10,
-                    takeRatePercent: 7.2
-                },
-                {
-                    baseMachineCode: '462FNW',
-                    baseMachineName: '4730 Sprayer Carb B',
-                    takeRateCount: 369,
-                    takeRatePercent: 61.1
-                },
-                {
-                    baseMachineCode: '-1',
-                    baseMachineName: 'Grand Total',
-                    takeRateCount: 580,
-                    takeRatePercent: 39.9
-                }
-            ]
-        },
-        {
-            optionGroupName: 'Ag Management Solutions Autotrac Activations',
-            optionCodeName: 'Less Autotrac Activation',
-            optionCode: '1845',
-            optionTakeRates: [
-                {
-                    baseMachineCode: '459ENW',
-                    baseMachineName: '4630 Sprayer',
-                    takeRateCount: 1,
-                    takeRatePercent: 0.4
-                },
-                {
-                    baseMachineCode: '4620NW',
-                    baseMachineName: '4730 Sprayer',
-                    takeRateCount: 79,
-                    takeRatePercent: 17.4
-                },
-                {
-                    baseMachineCode: '462ENW',
-                    baseMachineName: '4730 Sprayer Carb',
-                    takeRateCount: 0,
-                    takeRatePercent: 0
-                },
-                {
-                    baseMachineCode: '462FNW',
-                    baseMachineName: '4730 Sprayer Carb B',
-                    takeRateCount: 85,
-                    takeRatePercent: 14.1
-                },
-                {
-                    baseMachineCode: '-1',
-                    baseMachineName: 'Grand Total',
-                    takeRateCount: 165,
-                    takeRatePercent: 11.4
-                }
-            ]
-        },
-        {
-            optionGroupName: 'Auto Air Spring Leveling',
-            optionCodeName: 'Less Auto Air Spring Leveling',
-            optionCode: '7100',
-            optionTakeRates: [
-                {
-                    baseMachineCode: '459ENW',
-                    baseMachineName: '4630 Sprayer',
-                    takeRateCount: 14,
-                    takeRatePercent: 5.4
-                },
-                {
-                    baseMachineCode: '4620NW',
-                    baseMachineName: '4730 Sprayer',
-                    takeRateCount: 5,
-                    takeRatePercent: 1.1
-                },
-                {
-                    baseMachineCode: '462ENW',
-                    baseMachineName: '4730 Sprayer Carb',
-                    takeRateCount: 0,
-                    takeRatePercent: 0
-                },
-                {
-                    baseMachineCode: '462FNW',
-                    baseMachineName: '4730 Sprayer Carb B',
-                    takeRateCount: 0,
-                    takeRatePercent: 0
-                },
-                {
-                    baseMachineCode: '-1',
-                    baseMachineName: 'Grand Total',
-                    takeRateCount: 443,
-                    takeRatePercent: 30.5
-                }
-            ]
-        },
-        {
-            optionGroupName: 'Auto Air Spring Leveling',
-            optionCodeName: 'AUTO AIR SPRING LEVEL 5',
-            optionCode: '7110',
-            optionTakeRates: [
-                {
-                    baseMachineCode: '459ENW',
-                    baseMachineName: '4630 Sprayer',
-                    takeRateCount: 244,
-                    takeRatePercent: 94.6
-                },
-                {
-                    baseMachineCode: '4620NW',
-                    baseMachineName: '4730 Sprayer',
-                    takeRateCount: 448,
-                    takeRatePercent: 98.9
-                },
-                {
-                    baseMachineCode: '462ENW',
-                    baseMachineName: '4730 Sprayer Carb',
-                    takeRateCount: 138,
-                    takeRatePercent: 100
-                },
-                {
-                    baseMachineCode: '462FNW',
-                    baseMachineName: '4730 Sprayer Carb B',
-                    takeRateCount: 604,
-                    takeRatePercent: 100
-                },
-                {
-                    baseMachineCode: '-1',
-                    baseMachineName: 'Grand Total',
-                    takeRateCount: 19,
-                    takeRatePercent: 1.3
-                }
-            ]
-        }
-    ];
-};
-
-// export default staticMockData;
