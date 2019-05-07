67b1c420d1723edbe89e55fa5d0d1f4ac1bcc748
erictuftee <erictuftee@gmail.com>
2019-05-07 14:51:59 -0500

    chore: remove homepage from package

---
+++ build/precache-manifest.367ef55323dcf2df5c146732e71968e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71dbb78b89e48c8a310b42de6e1f1069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "116520d37dcf56593196",
+    "url": "/static/css/2.fee8f29b.chunk.css"
+  },
+  {
+    "revision": "4d047648017d5e17e7ab",
+    "url": "/static/css/main.25d72e10.chunk.css"
+  },
+  {
+    "revision": "116520d37dcf56593196",
+    "url": "/static/js/2.66871de9.chunk.js"
+  },
+  {
+    "revision": "4d047648017d5e17e7ab",
+    "url": "/static/js/main.ec78b4d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.453360bc226aa303ca6d3567e557912b.js"
+  "/precache-manifest.367ef55323dcf2df5c146732e71968e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
