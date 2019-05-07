8f30bd360fce8201c8e99fb9b75613f03c27aadb
erictuftee <erictuftee@gmail.com>
2019-05-07 15:20:04 -0500

    gh-pages working test

---
+++ build/precache-manifest.3739daf165ed26f270237b4bd5ba12ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90e54fd97db4812dcea6572adb8d908",
+    "url": "./index.html"
+  },
+  {
+    "revision": "116520d37dcf56593196",
+    "url": "./static/css/2.fee8f29b.chunk.css"
+  },
+  {
+    "revision": "2e8587ee26b5e018125c",
+    "url": "./static/css/main.d0a5c526.chunk.css"
+  },
+  {
+    "revision": "116520d37dcf56593196",
+    "url": "./static/js/2.66871de9.chunk.js"
+  },
+  {
+    "revision": "2e8587ee26b5e018125c",
+    "url": "./static/js/main.c4259b2e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
